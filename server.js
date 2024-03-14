f7a1b1129fd0e237e67a715b7ff43bf5ada1f081
Gokula <gokulakannanit@gmail.com>
2024-03-14 21:12:19 +0530

    express server added width index 11

--- server.js
+++ server.js
@@ -8,8 +8,4 @@ app.get("/*", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/service", (req, res) => {
-    res.sendFile(__dirname + "/index.html");
-  });
-
 http.listen(3000, () => console.log("listening on *:3000"));
